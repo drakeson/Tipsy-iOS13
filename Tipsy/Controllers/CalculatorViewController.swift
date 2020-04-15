013eb6d85d5b6bd5bad151ca6b2b827e9a20cdec
drakeson <ryansmith7119@gmail.com>
2020-04-15 06:53:12 +0300

    hello

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextF: UITextField!
+    @IBOutlet weak var zeroF: UIButton!
+    @IBOutlet weak var tenF: UIButton!
+    @IBOutlet weak var twentyF: UIButton!
+    @IBOutlet weak var spiltNum: UILabel!
+    var tip = 0.00
+    var numberOfPeople = 1
+    var billTotal = 0.0
+    var finalResult = "0.0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc override func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroF.isSelected = false
+        tenF.isSelected = false
+        twentyF.isSelected = false
+        
+        //Make the button that triggered the IBAction selected.
+        sender.isSelected = true
+        
+        //Get the current title of the button that was pressed.
+        let buttonTitle = sender.currentTitle!
+        
+        //Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        
+        //Turn the String into a Double.
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
+        tip = buttonTitleAsANumber / 100
+    }
+    
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        spiltNum.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculateP(_ sender: UIButton) {
+        let bill = billTextF.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+        }
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
